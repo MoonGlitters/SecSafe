554a0d2ca6e1a9036f2ed1e1fb5720c691d49e14
MoonGlitters <allen_nivek@hotmail.com>
2025-07-03 18:37:22 -0400

    Proyecto terminado

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,23 +1,41 @@
-
+const regexName = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const regexNumero = /^\+569\d{8}$/
 
 $(document).ready(()=> {
 
     const notyf = new Notyf()
-
+    
     $('#submit').click( (e) => {
         e.preventDefault()
-        const nombre = $('#nombre').value
-        const email = $('#email').value
-        const asunto = $('#asunto').value
-        const mensaje = $('#mensaje').value
-        const newsletter = $('#newletter').value
+        const nombre = $('#nombre').val()
+        const email = $('#email').val()
+        const telefono = $('#telefono').val()
+        const asunto = $('#asunto').val()
+        const mensaje = $('#mensaje').val()
 
-        if (!nombre || !email || !asunto || !mensaje || !newsletter) {
-            notyf.error('Todos los campos deben estar completados')
+        if (!nombre || !email || !asunto || !mensaje ) {
+            notyf.error('Todos los campos con * deben estar completados')
             return
         }
 
-        $('#contactForm').reset()
+        if(!regexName.test(nombre)) {
+            notyf.error('Nombre contiene caracteres invalidos')
+            return
+        }
+        if(!regexEmail.test(email)) {
+            notyf.error('Email contiene un formato invalido')
+            return
+        }
+
+        if(!regexNumero.test(telefono)) {
+            notyf.error('El numero contiene un formato no valido')
+            return
+        }
+
+        notyf.success('Consulta enviada con exito!')
+
+        $('#contactForm')[0].reset()
 
     })
 
@@ -26,8 +44,10 @@ $(document).ready(()=> {
         $('#textModal').modal('show');
     })
 
-
-
-
+    $('#submitEncuesta').click( (e) => {
+        e.preventDefault()
+        notyf.success('Encuesta enviada con exito')
+        $('#textModal').modal('hide');
+    })
 })
 
