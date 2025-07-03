b1979052ff5c317e19adad8ae43cee83838d1ba1
MoonGlitters <allen_nivek@hotmail.com>
2025-07-03 19:05:39 -0400

    Se agrega logica para cuestionario y readme

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -5,7 +5,7 @@ const regexNumero = /^\+569\d{8}$/
 $(document).ready(()=> {
 
     const notyf = new Notyf()
-    
+
     $('#submit').click( (e) => {
         e.preventDefault()
         const nombre = $('#nombre').val()
@@ -44,9 +44,37 @@ $(document).ready(()=> {
         $('#textModal').modal('show');
     })
 
+
+    const respuestasCorrectas = {
+        pregunta1: "b",
+        pregunta2: "b",
+        pregunta3: "b",
+        pregunta4: "b",
+        pregunta5: "b",
+        pregunta6: "b",
+        pregunta7: "b",
+        pregunta8: "b",
+        pregunta9: "b",
+        pregunta10: "b"
+    };
+
+    function evaluarRespuestas() {
+        let puntaje = 0;
+        const totalPreguntas = Object.keys(respuestasCorrectas).length;
+
+        $.each(respuestasCorrectas, function(pregunta, respuestaCorrecta) {
+            const respuestaSeleccionada = $("input[name='" + pregunta + "']:checked").val();
+            if (respuestaSeleccionada === respuestaCorrecta) {
+                puntaje++;
+            
+        }
+        }) 
+        notyf.success(`Obtuviste ${puntaje} de ${totalPreguntas} correctas`)
+        };
+
     $('#submitEncuesta').click( (e) => {
         e.preventDefault()
-        notyf.success('Encuesta enviada con exito')
+        evaluarRespuestas()
         $('#textModal').modal('hide');
     })
 })
